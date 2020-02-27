27d56a24c39c6a76fc0420e74e470ca79f39b92f
James Coman <jamespc44@gmail.com>
2020-02-26 23:33:29 -0600

    Fix sh in batch script

--- ada_batch.sh
+++ ada_batch.sh
@@ -5,9 +5,10 @@ cd "$(dirname "$0")" || exit
 mkdir -p lsf_output
 
 for f in traces/*.xz ; do
-
+	
+	FILE="$(basename "$f" )"
 	bsub -J "thisHurtsTheTLB" -L /bin/bash -W 0:10 -n 1 -R "span[ptile=1]"\
 	-R "rusage[mem=5000]" -M 5000 -o lsf_output/output.%J \
-	./run_champsim.sh bimodal-no-no-no-no-lru-1core 1 10 basename "$f"
+	./run_champsim.sh bimodal-no-no-no-no-lru-1core 1 10 basename "$FILE"
 done
 
