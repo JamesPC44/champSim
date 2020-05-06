e6686e8fa1c1538672e96753adb5243f8d7b778e
Coman, James <jamespc44@gmail.com>
2020-05-06 11:27:06 -0500

    Update batch script

--- ada_batch.sh
+++ ada_batch.sh
@@ -4,11 +4,16 @@ cd "$(dirname "$0")" || exit
 
 mkdir -p lsf_output
 
-for f in traces/*.xz ; do
-	
+
+for f in tlb_traces/*.xz ; do
+	(
 	FILE="$(basename "$f" )"
-	bsub -J "thisHurtsTheTLB" -L /bin/bash -W 0:10 -n 1 -R "span[ptile=1]"\
-	-R "rusage[mem=5000]" -M 5000 -o lsf_output/output.%J \
-	./run_champsim.sh bimodal-no-no-no-no-lru-1core 1 10 basename "$FILE"
+	printf "#BSUB -n 1 -R \"rusage[mem=5000] span[ptile=1]\" -M 5000
+#BSUB -J \"thisHurtsTheTLB\" -o lsf_output/output.%%J -L /bin/bash -W 0:10
+export CS_DO_PAGE_WALK=t
+./run_champsim.sh bimodal-no-no-no-no-lru-1core 1 10 %s" "$FILE" | bsub
+	)&
 done
 
+#export CS_PAGE_TABLE_LATENCY=125
+#export CS_DO_PAGE_WALK=t
